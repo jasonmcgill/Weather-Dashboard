c79d2edd66982a3d6062c362efa49a9cf625c3e0
jasonmcgill <64054045+jasonmcgill@users.noreply.github.com>
2020-06-06 22:29:28 -0600

    added more psuedo code, and all fetch apis for pulling the default city on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,33 +5,77 @@ var submitButtonEl = document.querySelector("#city-submit")
 
 
 
-
 //handler for current weather submit button currentWeather
 var currentWeather = function (city) {
     var apiUrl5Day = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&apikey=b2f5c5fd56830b2ca51bd32529509771"
 
     fetch(apiUrl5Day)
         .then(function (response) {
+            //if we have input in the field
             if (response.ok) {
-                response.json().then(function (data) {});
-            } else {
+                response.json()
+                    .then(function (data) {                        
+                        //add the values into their respective html spans
+                        $("#default-city").text(data.city.name);
+                        $("#default-date").text(moment().format('MMMM Do, YYYY'));
+                        $("#default-temperature").text(Math.round(data.list[0].main.temp) + "°");
+                        $("#default-humidity").text(data.list[0].main.humidity);
+                        $("#default-wind-speed").text(data.list[0].wind.speed);
+                        //adds the icon
+                        var iconcode = data.list[0].weather[0].icon;
+                        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                        $('#wicon').attr('src', iconurl);
+
+                        //new api that has UV index, contatenated with latitude and longitude from previus fetch
+                        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&apikey=b2f5c5fd56830b2ca51bd32529509771")
+                        .then(function (response) {
+                            response.json()
+                                .then(function (data) {
+                                    //depending on the UV index display background color of how favorable it is
+                                    if (data.current.uvi <= 3)  {
+                                        $("#default-uv-index").text(data.current.uvi);
+                                        $("#default-uv-index").addClass("text-white bg-success")
+                                    }
+                                    if (data.current.uvi > 4 && data.current.uvi < 7 )  {
+                                        $("#default-uv-index").text(data.current.uvi);
+                                        $("#default-uv-index").addClass("text-white bg-warning")
+                                    }
+                                    if (data.current.uvi > 8 )  {
+                                        $("#default-uv-index").text(data.current.uvi);
+                                        $("#default-uv-index").addClass("text-white bg-danger");                                        
+                                    }
+                                                                
+                                });
+                        });
+                    });
+                //if we get error from server display their error
+                } else {
                 alert("Error: " + response.statusText);
             }
+
         })
         .catch(function (error) {
             alert("Unable to connect to Weather");
         });
     
-    $
-
-    //change current weather data in main area
-    //call 5dayForcast(with submit buttons input) 
+    fiveDayForcast(city);      
 }
-    
+var fiveDayForcast = function (city) {
+        // fetch apiUrl5Day
+        var apiUrl5Day = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&apikey=b2f5c5fd56830b2ca51bd32529509771"
 
-//handler for 5dayForcast var 5dayForcast = function (city) {}
-    // fetch apiUrl5Day
-    // append data to 5 day forcast container
+        fetch(apiUrl5Day)
+            .then(function (response) {
+                    if (response.ok) {
+                        response.json()
+                            .then(function (data) {
+                                    console.log(data);
+                                })
+                                // append data to 5 day forcast container
+                    }
+                    });
+                };
+
 
 
 //save city to local storage
@@ -44,7 +88,7 @@ var currentWeather = function (city) {
     //append items
 
 // getCityHistory();
-// currentWeather(Anaheim);
+currentWeather("Anaheim");
 
 var buttonClickHandler = function(event){
     var city = event.value
